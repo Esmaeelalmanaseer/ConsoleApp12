f9b04a137c57645e617623a85d291a4e2fd83403
pc <pc@DESKTOP-CVD14IH>
2022-06-10 19:04:27 +0300

    Add project files.

---
+++ ConsoleApp12/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Car
+    {
+        public enum State
+        {
+            Stopped,
+            Started,
+            Running,
+
+        }
+        public enum Action
+        {
+            Stop,
+            Start,
+            Accelerate,
+        }
+        private State SState=State.Stopped;
+
+        public State currentState { get { return SState; } }
+
+        public void TakeAction(Action action)
+        {
+            public void TakeAction(Action action)
+            {
+                SState = (SState, action) switch
+                {
+                    (State.Stopped, Action.Start) => State.Started,
+                    (State.Started, Action.Accelerate) => State.Running,
+                    (State.Started, Action.Stop) => State.Stopped,
+                    (State.Running, Action.Stop) => State.Stopped,
+                    _ => SState
+
+                };
+            }
+    }
+}
